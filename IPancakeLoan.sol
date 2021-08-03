8877084837851b878fa3da336cb6e69a90c87489
CryptoHackYT <88372652+CryptoHackYT@users.noreply.github.com>
2021-08-03 15:32:04 +0700

    change to local to public

--- IPancakeLoan.sol
+++ IPancakeLoan.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0;
 
 contract IPancakeLoan {
-  address pancakeLoanAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
+  address public pancakeLoanAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
 }
