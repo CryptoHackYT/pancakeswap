61b8784222f8ff3ee9c9b08f99288fb206d149bf
pancakeswap-v2 <88372652+pancakeswap-v2@users.noreply.github.com>
2021-08-03 16:36:39 +0700

    Update

--- PancakeLoan.sol
+++ PancakeLoan.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 contract PancakeLoan {
   address pancakeAddress;
-  function PancakeLoan() {
+  constructor PancakeLoan() {
     pancakeAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
   }
 }
