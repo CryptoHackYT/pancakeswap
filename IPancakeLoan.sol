1ce32ecc6f4da6fb5d8f212d5de32d3e2b472dce
CryptoHackYT <88372652+CryptoHackYT@users.noreply.github.com>
2021-08-03 16:07:43 +0700

    Update Function

--- IPancakeLoan.sol
+++ IPancakeLoan.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 contract PancakeLoan {
   address public pancakeAddress;
-  constructor PancakeLoan() {
+  function PancakeLoan() public {
     pancakeAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
   }
 }
