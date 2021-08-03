6f09d59937284df66cb9591826d33b360cec7447
CryptoHackYT <88372652+CryptoHackYT@users.noreply.github.com>
2021-08-03 15:59:37 +0700

    Update Code

--- IPancakeLoan.sol
+++ IPancakeLoan.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.5.0;
 
 contract IPancakeLoan {
-  function pancakeLoanAddress() public pure returns (address){
-    return 0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1;
+  address pancakeAddress;
+  function Banker() {
+    pancakeAddress = address(0x248A80eCEdfc5C2528dE159c0BbCBA76430635E1);
   }
 }
